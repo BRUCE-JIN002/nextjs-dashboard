34986582fa53b00c1dc9bee12a1f58697e9d294f
jinxianxiang <1633764493@qq.com>
2024-08-21 11:41:34 +0800

    add search and pagination

--- next.config.mjs
+++ next.config.mjs
@@ -1,5 +1,8 @@
 /** @type {import('next').NextConfig} */
 
-const nextConfig = {};
-
+const nextConfig = {
+  experimental: {
+    ppr: "incremental" //The 'incremental' value allows you to adopt PPR for specific routes.
+  }
+};
 export default nextConfig;
